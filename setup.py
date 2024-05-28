76ed418367d13bd27552142e02e03bf43950d78b
Ruggero Cortini <ruggero.cortini@gmail.com>
2024-05-28 15:30:36 +0200

    Update setup.py: read requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools.command.install import install
 import subprocess
 import os
 
+def read_requirements():
+    """Read the requirements.txt file and return a list of requirements."""
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
@@ -15,9 +20,7 @@ setup(
     name='pybruno',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        # your python dependencies
-    ],
+    install_requires=read_requirements(),
     cmdclass={
         'install': PostInstallCommand,
     },
